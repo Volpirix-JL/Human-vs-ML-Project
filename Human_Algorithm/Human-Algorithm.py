934722f74d1c6aeda338c228f28f428b59d04675
Volpirix-JL <s978859@student.knoxschools.org>
2026-02-10 21:02:37 +0000

    Co-authored-by: CARSON HARREL (S941317) <S941317@student.knoxschools.org>

---
+++ Human_Algorithm/Human-Algorithm.py
@@ -0,0 +1,5 @@
+'''
+
+'''
+
+df = pd.read_csv("/content/top_10000_1950-now.csv")
---
+++ Human_Algorithm/fetch_data.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+import matplotlib as plt
+
+df = pd.read_csv("/workspaces/Human-vs-ML-Project/Human_Algorithm/apple_quality.csv") 
