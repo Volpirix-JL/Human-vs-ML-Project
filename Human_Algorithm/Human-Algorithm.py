388bf935bc6c72dc6e74afac23c3afd12f0c245e
Volpirix-JL <s978859@student.knoxschools.org>
2026-02-17 21:02:28 +0000

    added images, updated fetch function

--- Human_Algorithm/Human-Algorithm.py
+++ Human_Algorithm/Human-Algorithm.py
@@ -2,4 +2,4 @@
 
 '''
 
-df = pd.read_csv("/content/top_10000_1950-now.csv")
+
--- Human_Algorithm/fetch_data.py
+++ Human_Algorithm/fetch_data.py
@@ -1,4 +1,22 @@
-import pandas as pd 
-import matplotlib as plt
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
 
 df = pd.read_csv("/workspaces/Human-vs-ML-Project/Human_Algorithm/apple_quality.csv") 
+dfG = df[df['Quality'] == "good"].head(500)
+dfB = df[df['Quality'] == "bad"].head(500)
+# print(df.columns)
+
+plt.scatter(dfG['Size'], dfG['Weight'], color = 'green', label = "Good")
+plt.scatter(dfB['Size'], dfB['Weight'], color='blue', label = "Bad")
+plt.xlabel("Size")
+plt.ylabel("Weight")
+plt.title("Apple Quality: Size vs Weight")
+plt.legend()
+
+plt.savefig("Size_v_Weight.png", dpi=150)
+
